15c2caa6117321ea383aa62def8fe41c38462612
Luis Aneuris Tavarez De Jesus <20186927@itla.edu.do>
2024-07-20 15:44:59 -0400

    edit commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-console.log('LOCAL___hello----sabado-2-2024-C2/index.js ---LOCAL___hello');
-console.log('----LOCAL___hello world----LOCAL___hello-----');
+console.log('----sabado-2-2024-C2--------------');
+console.log('----LOCAL---------');
 console.log('sabado-2-2024-C2/index.js');
 console.log('hello world');
+console.log('GITHUB----sabado-2-2024-C2/index.js ---GITHUB');
+console.log('hello world----GITHUB-----');
